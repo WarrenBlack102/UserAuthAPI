847e4691581024827f7924af40f9142cc1ace8ee
WarrenBlack102 <warrenblack102@outlook.com>
2022-05-11 16:32:11 -0400

    endpoints add

--- app.py
+++ app.py
@@ -16,6 +16,70 @@ ma = Marshmallow(app)
 bcrypt = Bcrypt(app)
 CORS(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, nullable = False, unique = True)
+    password = db.Column(db.String, nullable = False)
+    email = db.Column(db.String, unique = True)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "username", "password", "email")
+
+user_schema = UserSchema()
+multi_user_schema = UserSchema(many=True)
+
+
+@app.route("/user/add", methods=["POST"])
+def add_user():
+    if request.content_type != "application/json":
+        return jsonify("ERROR: JSON needed here")
+
+    post_data = request.get_json()
+    username = post_data.get("username")
+    password = post_data.get("password")
+    email = post_data.get("email")
+
+    pw_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+        
+    new_record = User(username, pw_hash, email, password)
+    db.session.add(new_record)
+    db.session.commit()
+
+    return jsonify(user_schema.dump(new_record))
+
+
+@app.route("/user/verification", methods=["POST"])
+def verification():
+    if request.content_type != "application/json":
+        return jsonify("User could not be Verfied!")
+
+    post_data = request.get_json()
+    username = post_data.get("username")
+    password = post_data.get("password")
+
+    user = db.session.query(User).filter(User.username == username).first()
+
+    if user is None:
+        return jsonify("User could not be Verfied!")
+
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify("User could not be Verfied!")
+
+    return jsonify("User Verified")
+
+
+@app.route("/user/get", methods=["GET"])
+def get_users():
+    users = db.session.query(User).all()
+    return jsonify(multi_user_schema.dump(users))
+
 
 
 
